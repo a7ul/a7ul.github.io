65866f8f109f19a298c9c8e31196de9241123683
Atul <atul_r@external.mckinsey.com>
2015-08-02 18:40:31 +0530

    Bootstrap navbar B)

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the websiteApp
+ */
+angular.module('websiteApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the websiteApp
+ */
+angular.module('websiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
