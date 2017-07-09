b8fd6968ebd7cd22425c31a2ea935829ad0df2ca
Atul <atulanand94@gmail.com>
2017-07-09 13:16:14 +0530

    projects ui

---
+++ app/components/Home/Projects.component.js
@@ -0,0 +1,21 @@
+import Icon from '../Icon/Icon.component';
+import Touchable from 'rc-touchable';
+import React, {Component} from 'react';
+import styles from './Projects.component.style';
+
+class Projects extends Component {
+  render () {
+    return (
+      <div style={styles.container}>
+        <div>
+          <a href='https://github.com/master-atul' rel='noopener noreferrer' target='_blank' style={styles.linkContainer}><Touchable activeStyle={styles.link}><Icon style={styles.icon} size={25} name='github'/></Touchable></a>
+          <a href='https://www.linkedin.com/in/atulanand94' rel='noopener noreferrer' target='_blank' style={styles.linkContainer}><Touchable activeStyle={styles.link}><Icon style={styles.icon} size={25} name='linkedin'/></Touchable></a>
+          <a href='mailto:atulanand94@gmail.com' rel='noopener noreferrer' target='_blank' style={styles.linkContainer}><Touchable activeStyle={styles.link}><Icon style={styles.icon} size={25} name='envelope'/></Touchable></a>
+          <a href='https://twitter.com/masteratul94' rel='noopener noreferrer' target='_blank' style={styles.linkContainer}><Touchable activeStyle={styles.link}><Icon style={styles.icon} size={25} name='twitter'/></Touchable></a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Projects;
