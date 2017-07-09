ec90f35a463a39c4d99a76c64f76e8660aa16663
Atul <atulanand94@gmail.com>
2017-07-09 19:04:17 +0530

    added few more scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="src"
-TARGET_BRANCH="master"
-GITHUB_API_V4_READ_TOKEN=''
+SOURCE_BRANCH="feature/revamp"
+TARGET_BRANCH="test"
+GITHUB_API_V4_READ_TOKEN=""
 
 function updateProjects {
   node scripts/projects_updater.js ${GITHUB_API_V4_READ_TOKEN} &&
-  yarn build &&
+  yarn build
 }
-
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -25,7 +24,7 @@ updateProjects
 cp -rf bundle ../project_dist
 
 git checkout $TARGET_BRANCH
-rm -rf ./**/*
+# rm -rf ./**/*
 cp -rf ../project_dist/* .
 
 git config user.name "Atul R"
@@ -43,14 +42,14 @@ git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+# chmod 600 ../deploy_key
+# eval `ssh-agent -s`
+# ssh-add deploy_key
+#
+# # Now that we're all set up, we can push.
+# git push $SSH_REPO $TARGET_BRANCH
