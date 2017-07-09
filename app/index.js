fda566a9ca3745790d074902f6270c2c81204a19
Atul <atulanand94@gmail.com>
2017-07-10 01:33:01 +0530

    changes

--- app/index.js
+++ app/index.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {initStore} from './state/store';
+import contributions from './assets/json/contributions.json';
+import projects from './assets/json/projects.json';
+
+console.log('contributions, projects', contributions, projects);
 
 const store = initStore({});
 
