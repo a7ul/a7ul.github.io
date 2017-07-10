f053ad3dff89d4bc0969ed1cba66950263c95a28
Atul R <atulanand94@gmail.com>
2017-07-10 21:41:01 +0530

    create all branches locally

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,12 +10,37 @@ function updateProjects {
   yarn build
 }
 
+function create_all_branches {
+    # Keep track of where Travis put us.
+    # We are on a detached head, and we need to be able to go back to it.
+    local build_head=$(git rev-parse HEAD)
+
+    # Fetch all the remote branches. Travis clones with `--depth`, which
+    # implies `--single-branch`, so we need to overwrite remote.origin.fetch to
+    # do that.
+    git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+    git fetch
+    # optionally, we can also fetch the tags
+    git fetch --tags
+
+    # create the tacking branches
+    for branch in $(git branch -r|grep -v HEAD) ; do
+        git checkout -qf ${branch#origin/}
+    done
+
+    # finally, go back to where we were at the beginning
+    git checkout ${build_head}
+}
+
+
 # Save some useful information
 REPO="https://github.com/master-atul/master-atul.github.io.git"
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 git config user.name "Atul R"
 git config user.email "atulanand94@gmail.com"
+
+create_all_branches &&
 
 git checkout $SOURCE_BRANCH
 
