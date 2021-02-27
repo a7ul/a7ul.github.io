fc24d4b89f9e6af733b76268ed03bcfcd47b8982
Atul R <atulanand94@gmail.com>
2021-02-27 20:31:55 +0100

    bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+interface AppProps {}
+
+function App(props: AppProps) {
+  return (
+    <div className="App">
+      <header className="App-header">Yo</header>
+    </div>
+  );
+}
+
+export default App;
