14d6601b6d38377e05981e31d01184cc659fd97f
Atul R <atulanand94@gmail.com>
2021-07-21 23:36:39 +0200

    bump packages

--- docs/_snowpack/pkg/react.js
+++ docs/_snowpack/pkg/react.js
@@ -1,2 +1,2 @@
-import { r as react } from './common/index-e66f0a38.js';
-export { r as default } from './common/index-e66f0a38.js';
+import { r as react } from './common/index-ae389540.js';
+export { r as default } from './common/index-ae389540.js';
